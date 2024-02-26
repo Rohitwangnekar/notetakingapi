dcabb904ad28ff7247c7161dd60c5ab581038c5a
Rohitwangnekar <103194792+Rohitwangnekar@users.noreply.github.com>
2024-02-26 10:52:27 +0530

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+// routes/auth.js
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.create({ username, password });
+    res.status(201).json({ message: 'User registered successfully', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = jwt.sign({ username }, 'your-secret-key');
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = router;
